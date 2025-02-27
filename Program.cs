1b174028209ce773ddb187bc03bc078e0e8eb7c2
Clara Crastechini <ccrastechini@gmail.com>
2025-02-27 08:39:46 -0300

    feat: Subindo atualizações sobre o arquivo/tabela filmes

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using api_filmes_senai.Context;
+using api_filmes_senai.Interfaces;
+using api_filmes_senai.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Adiciona o contexto do banco de dados (exemplo com SQL Server)
+builder.Services.AddDbContext<Filmes_Context>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+//Adicionando o repositorio e a interface ao container de injecao de dependencia
+builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
+
+//Adicionar o servico de controllers
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IFilmeRepository, FilmeRepository>();
+
+var app = builder.Build();
+
+//Adicionar o mapeamento dos controllers
+app.MapControllers();
+
+app.Run();
+
---
+++ Repositories/FilmeRepository.cs
@@ -0,0 +1,118 @@
+﻿using System.Linq;
+using api_filmes_senai.Context;
+using api_filmes_senai.Domains;
+using api_filmes_senai.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_filmes_senai.Repositories
+{
+
+    /// <summary>
+    /// Classe que vai implementar a interface IFilmeRepository
+    /// Ou seja,vamos implementar os metodos, toda a logicas dos metodos
+    /// </summary>
+    public class FilmeRepository : IFilmeRepository
+    {
+        private readonly Filmes_Context _context;
+        public FilmeRepository(Filmes_Context context) {
+            _context = context;
+        }
+        public void Atualizar(Guid id, Filme filme)
+        {
+            Filme filmeBuscado = _context.Filme.Find(id);
+
+            if (filmeBuscado != null)
+            {
+                filmeBuscado.Titulo = filme.Titulo;
+                filmeBuscado.IdGenero = filme.IdGenero;
+            }
+
+            _context.SaveChanges();
+        }
+
+        public Filme BuscarPorId(Guid id)
+        {
+            try
+            {
+               Filme filmeBuscado = _context.Filme.Find(id)!;
+               return filmeBuscado;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Cadastrar(Filme novoFilme)
+        {
+            try
+            {
+               _context.Filme.Add(novoFilme);
+               _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filme.Find(id)!;
+                if (filmeBuscado != null)
+                {
+                    _context.Filme.Remove(filmeBuscado);  
+                }  
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Filme> Listar()
+        {
+            try
+            {
+                List<Filme> listaDeFilmes = _context.Filme.Include(g => g.Genero).ToList();
+                //.Select(f => new Filme){
+                //IdFilme = f.IdFilme,
+                //Titulo = f.Titulo,
+                //Genero = new Genero
+                //{
+                //   idGenero = f.IdGenero,
+                //   Nome = f.Genero!.Nome
+                //}
+                
+                return listaDeFilmes;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public List<Filme> ListarPorGenero(Guid IdGenero)
+        {
+            try
+            {
+                List<Filme> listaDeFilmesGenero = _context.Filme.Include(g => g.Genero)
+                    .Where(f => f.IdGenero == IdGenero) //aqui vc tem que fazer a condicao
+                    .ToList();   
+                return listaDeFilmesGenero;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
